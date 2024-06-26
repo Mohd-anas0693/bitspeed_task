ca5cd4f6b0c0740fb09957a044450546136e11cb
Mohd-anas0693 <mohdanas0.693@gmail.com>
2024-06-26 11:07:44 +0530

    Modification : Create the logic and criteia to create new Contract and create secondary contact if there is primary contact

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -1,15 +1,66 @@
+const { Op } = require("sequelize");
 const Contact = require('../models/contact.model');
 const asyncHandler = require('../utils/asyncHandler')
 const ApiResponse = require('../utils/apiResponse');
+const ApiErrors = require('../utils/apiErrors');
+function createResponse(primaryContactId, emails, phoneNumbers, secondaryContactIds) {
+    return {
+        contact: {
+            primaryContactId: primaryContactId,
+            emails: emails,
+            phoneNumbers: phoneNumbers,
+            secondaryContactIds: secondaryContactIds,
+        }
+    }
+};
 module.exports = {
     createContact: asyncHandler(async (req, res) => {
-        const userData = await Contact.create({
-            phoneNumber: "123",
-            email: "1234@gmail.com",
-            linkedId: null,
-            linkPrecedence: "primary",
-        })
-        console.log(userData)
-        return res.status(200).json(new ApiResponse(200, userData, "yes"));
+
+        const { userPhoneNumber, userEmail } = req.body;
+
+
+        if ([userPhoneNumber, userEmail].some((fields) => fields == undefined || "")) {
+            throw new ApiErrors(400, "Error! Missing fields");
+        }
+
+        const condition = {
+            [Op.or]: [
+                { phoneNumber: userPhoneNumber },
+                { email: userEmail }
+            ]
+        }
+
+        const existingContacts = await Contact.findAll({ where: condition });
+        // console.log("ExistingContact : ", existingContacts);
+
+        let newContact;
+        if (existingContacts.length === 0 || !existingContacts[0]) {
+
+            newContact = await Contact.create({ phoneNumber: userPhoneNumber, email: userEmail, linkedId: null, linkPrecedence: "primary" });
+
+            if (!newContact) throw new ApiErrors(500, "Server Error! Insertion Failed.");
+
+            const { id, email, phoneNumber, linkedId } = newContact.toJSON();
+
+            return res.status(200).json(new ApiResponse(200, createResponse(id, [email], [phoneNumber], linkedId), "yes"));
+        }
+        // console.log('ExistingContacts:', existingContacts)
+        const primaryContact = existingContacts.find(obj => obj.toJSON().linkedId == null && obj.toJSON().linkPrecedence === "primary")
+
+        // console.log("Primary Contact: ", primaryContact.toJSON());
+
+        const newSecondaryContact = await Contact.create({
+            phoneNumber: userPhoneNumber,
+            email: userEmail,
+            linkedId: primaryContact.id,
+            linkPrecedence: "secondary"
+        });
+
+        if (!newSecondaryContact) throw new ApiErrors(500, "Not able Insert value in Database");
+        console.log("newSecondaryContact : ", newSecondaryContact.toJSON());
+        const allContacts = [...existingContacts, newSecondaryContact];
+
+        // console.log(allContacts)
+        return res.status(200).json(new ApiResponse(200, allContacts, "yes"));
     })
 }
